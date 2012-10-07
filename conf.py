5b618474f23c2673284cf5a1403955e99983b2e5
Gordon Guthrie <gordon@vixo.com>
2012-10-07 11:46:07 +0100

    make docos look like the blog

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Building Applications With Vixo'
-copyright = u'2011, Hypernumbers Ltd (trading as vixo.com)'
+copyright = u'2012, Hypernumbers Ltd (trading as vixo.com)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
