e7bfc4ee37dabca0114e6a56b467d9abb62dc94d
Gordon Guthrie <gordon@vixo.com>
2012-10-07 21:45:59 +0100

    fixed up unpacking batch file

--- unpack_doco.sh
+++ unpack_doco.sh
@@ -5,5 +5,7 @@ mkdir documentation
 echo "unzipping and unpacking new contents"
 cd ./documentation/
 gunzip ../doco.tar.gz
-tar -xvf doco.tar
+tar -xvf ../doco.tar
+mkdir files
+cp ../BuildingApplicationsWithVixo.pdf ./files
 echo "over and out..."
