ade28205c7b1e0f0d0821fcf8702aa0c4e5cb0ee
Gordon <gordon@hypernumbers.com>
2012-11-26 09:19:57 +0000

    erk, uncomment out build stuff

--- make_and_upload.sh
+++ make_and_upload.sh
@@ -22,25 +22,25 @@ echo "" >> ./contents/welcome/version.rst
 echo "Version: $newrelease" >> ./contents/welcome/version.rst
 echo "Generated: $now" >> ./contents/welcome/version.rst
 echo "making html"
-#make html
+make html
 echo "making latex"
-#make latex
+make latex
 echo "making epub"
-#make epub
+make epub
 cd ./_build/epub
-#ebook-convert BuildingApplicationsWithVixo.epub BuildingApplicationsWithVixo.mobi
-#scp BuildingApplicationsWithVixo.* gordon@hypernumbers.com:/hn/files-www/vixo2/
+ebook-convert BuildingApplicationsWithVixo.epub BuildingApplicationsWithVixo.mobi
+scp BuildingApplicationsWithVixo.* gordon@hypernumbers.com:/hn/files-www/vixo2/
 cd ../latex
 echo "making pdf"
-#make all-pdf
+make all-pdf
 echo "uploading pdf"
-#scp BuildingApplicationsWithVixo.pdf gordon@hypernumbers.com:/hn/files-www/vixo2/
+scp BuildingApplicationsWithVixo.pdf gordon@hypernumbers.com:/hn/files-www/vixo2/
 cd ../html
 echo "tarring and zipping html"
-#rm doco.tar.gz
-#tar -cvf doco.tar *
-#gzip doco.tar
+rm doco.tar.gz
+tar -cvf doco.tar *
+gzip doco.tar
 echo "uploading zipped html"
-#scp doco.tar.gz gordon@hypernumbers.com:/hn/files-www/vixo2/
+scp doco.tar.gz gordon@hypernumbers.com:/hn/files-www/vixo2/
 cd /home/gordon/manual
 echo "over and out..."
